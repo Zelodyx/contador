926af0649ceca3fe386323479cdf27d4ac988875
Zelody <zelody@Equipo1.com>
2023-02-28 22:11:59 -0600

    Crear contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+
 function App() {
+  const [Count, setCount] = useState(0)
+
+  return(
+    <div>
+      <h3>Contador</h3>
+      <hr />
+      <h1>{Count}</h1>
+      <button onClick={() => {setCount(Count+1);
+      console.log(Count);}}
+      type="button" 
+      className="btn btn-primary">+1
+      </button>
+      <button onClick={() => {setCount(Count-1);
+      console.log(Count);}}
+      type="button" 
+      className="btn btn-primary">-1
+      </button>
+      <button onClick={() => {setCount(Count*0);
+      console.log(Count);}}
+      type="button" 
+      className="btn btn-primary">= 0
+      </button>
+    </div>
+  )  
 }
 
 export default App
