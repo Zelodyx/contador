fc62223dbdbfa8d91ca3e9850bc5458f1378ee32
Zelody <zelody@Equipo1.com>
2023-03-01 16:27:19 -0600

    Actualizacion de Botones y Alineación

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ function App() {
   const [Count, setCount] = useState(0)
 
   return(
-    <div>
+    <div align="center" className="mt-3">
       <h3>Contador</h3>
       <hr />
-      <h1>{Count}</h1>
+      <h1 className="mb-3">{Count}</h1> 
       <button onClick={() => {setCount(Count+1);
       console.log(Count);}}
       type="button" 
-      className="btn btn-primary">+1
+      className="btn btn-primary m-1">+1
       </button>
       <button onClick={() => {setCount(Count-1);
       console.log(Count);}}
       type="button" 
-      className="btn btn-primary">-1
+      className="btn btn-primary m-1">-1
       </button>
-      <button onClick={() => {setCount(Count*0);
+      <button onClick={() => {setCount(0);
       console.log(Count);}}
       type="button" 
-      className="btn btn-primary">= 0
+      className="btn btn-primary m-1">0
       </button>
     </div>
   )  
